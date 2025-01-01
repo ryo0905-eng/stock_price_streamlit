e577bb36c2b78a9dac12cd00eafc4b93276d1fad
ryo0905-eng <148692843+ryo0905-eng@users.noreply.github.com>
2025-01-01 16:50:12 +0800

    streamlit用に修正

--- app.py
+++ app.py
@@ -1,47 +1,59 @@
-# %%
+
 import yfinance as yf
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
+import matplotlib.pyplot as plt
+from pycaret.time_series import *
 
+# タイトル
+st.title('S&P500 Stock price predictions')
 
 # S&P500のデータを取得
 ticker = "^GSPC"
-data = yf.download(ticker, start="2010-01-01", end="2024-12-31")
+data = yf.download(tickers=ticker, start="2020-01-01", end="2024-12-31", multi_level_index=False)
 
-# 特徴量とターゲットの設定
-data['Date'] = data.index
-data['Date'] = pd.to_datetime(data['Date'])
-data['Date'] = data['Date'].map(pd.Timestamp.toordinal)
-X = data[['Date']]
-y = data['Close']
+# datetime形式に変換
+data.index = pd.to_datetime(data.index)
 
+# 欠損値は直前の値で補完
+data = data.asfreq('D', method='bfill')
 
-# 訓練データとテストデータに分割
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# データの確認
+st.write('データの先頭5行を表示')
+st.write(data.head())
 
-# 線形回帰モデルの作成と訓練
-model = LinearRegression()
-model.fit(X_train, y_train)
+# 予測に使わない特徴量を指定
+ignore_features = ['High', 'Low', 'Open', 'Volume']
+
+# モデルのセットアップ
+s = setup(data, fh = 30, fold = 5, session_id = 123, target='Close', ignore_features=ignore_features)
+
+#　モデルを作成
+arima = create_model('arima')
+
+# 交差検証結果を表示
+arima_results = pull()
+st.write('交差検証結果')
+st.write(arima_results)
 
 # 予測
-y_pred = model.predict(X_test)
+pred = predict_model(arima)
 
-# モデルの評価
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
+# Datetime形式に変換
+pred.index = pred.index.to_timestamp()
+pred.head()
 
-# 予測結果の表示
-import matplotlib.pyplot as plt
+# 実際の値をプロット
+fig = px.line(data, x=data.index, y='Close', title='S&P500 Stock price predictions')
 
-plt.figure(figsize=(10, 5))
-plt.plot(data.index, data['Close'], label='Actual')
-plt.plot(X_test.index, y_pred, label='Predicted', linestyle='--')
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.title('S&P500 Close Price Prediction')
-plt.legend()
-st.pyplot(plt)
+# 予測値をプロット
+predicted_trace = go.Scatter(x=pred.index, y=pred['y_pred'], mode='lines', name='Predicted')
+fig.add_trace(predicted_trace)
+
+# 色を変更
+fig.update_traces(line_color='red', selector=dict(name='Predicted'))
+
+# グラフを表示
+st.plotly_chart(fig)
