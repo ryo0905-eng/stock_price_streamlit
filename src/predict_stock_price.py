af470eee42b45abba7a10e78478b80c9e5cc97b1
ryo0905-eng <148692843+ryo0905-eng@users.noreply.github.com>
2025-01-04 07:54:19 +0800

    ログの出力を削除

--- src/predict_stock_price.py
+++ src/predict_stock_price.py
@@ -1,14 +1,5 @@
 from pycaret.time_series import *
 
-import logging
-
-# ログの設定
-logging.basicConfig(
-                    level=logging.INFO,
-                    filename='log/saved_model.log',
-                    format='%(asctime)s:%(levelname)s:%(message)s'
-                    )
-
 def predict_stock_price(model_dict, model_name):
     #　モデルを作成
     model = create_model(model_dict[model_name])
@@ -24,8 +15,5 @@ def predict_stock_price(model_dict, model_name):
 
     # モデルをsave
     save_model(model, f'saved_model/{model_name}')
-
-    # ログを出力
-    logging.info(f'{model_name} model is saved')
 
     return pred
